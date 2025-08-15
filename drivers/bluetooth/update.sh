638b1b8498573954c7582059c97fd959057bd51c
Dwi Asmoro Bangun <dwiaceromo@gmail.com>
2025-08-16 05:04:19 +0700

    adjust to linux 6.16

--- drivers/bluetooth/update.sh
+++ drivers/bluetooth/update.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
 KERNEL_SEMVER=$(uname -r | cut -d- -f1)
+PATCH_VER=$(echo "$KERNEL_SEMVER" | cut -d. -f3)
+
+if [ "$PATCH_VER" = '0' ]; then
+    KERNEL_SEMVER=$(echo "$KERNEL_SEMVER" | awk -F ".0" '{print $1}')
+fi
 
 for i in $(echo "intel bcm rtl mtk" | sed 's/ /\n/g'); do
 	curl -O "https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/plain/drivers/bluetooth/bt$i.h?h=v$KERNEL_SEMVER"
